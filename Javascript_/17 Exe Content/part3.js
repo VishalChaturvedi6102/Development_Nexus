586abd0abcbc54514668dc58a18c67a9723c771c
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2025-06-25 23:15:26 +0530

    commit

---
+++ Javascript_/17 Exe Content/part3.js
@@ -0,0 +1,14 @@
+
+
+// Memory allocation pehle hota hai isiliye -->
+
+greet(); // ye execute hoo jayega kyuki greet wala store hoo chuka hai.
+function greet(){
+    console.log("Hello Bhaiya");
+}
+
+meet(); // error because let contains nothing as initialized.
+let meet = function(){
+    console.log("Hello Ji");
+}
+meet(); // Perfectly executed.

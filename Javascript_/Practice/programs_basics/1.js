7b3a7b3338db4b157677369d37c5991ad2cca867
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2025-11-13 23:31:32 +0530

    event emittor + deep equal

--- Javascript_/Practice/programs_basics/1.js
+++ Javascript_/Practice/programs_basics/1.js
@@ -101,3 +101,65 @@ Promise.all([promise1, promise2, promise3])
     console.error('Error:', error);
   });
 
+
+
+
+
+  // deepequal
+  function deepEqual(obj1, obj2) {
+  if (obj1 === obj2) return true;
+
+  if (
+    obj1 == null ||
+    typeof obj1 !== 'object' ||
+    obj2 == null ||
+    typeof obj2 !== 'object'
+  )
+    return false;
+
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) return false;
+  }
+
+  return true;
+}
+
+// Example usage
+const object1 = {
+  name: 'John',
+  age: 30,
+  address: {
+    city: 'New York',
+    zip: '10001',
+  },
+};
+
+const object2 = {
+  name: 'John',
+  age: 30,
+  address: {
+    city: 'New York',
+    zip: '10001',
+  },
+};
+
+console.log(deepEqual(object1, object2)); // true
+
+
+
+
+// event emitter
+const eventEmitter = new EventEmitter();
+
+// Subscribe to an event
+eventEmitter.on('customEvent', (data) => {
+  console.log('Event emitted with data:', data);
+});
+
+// Emit the event
+eventEmitter.emit('customEvent', { message: 'Hello, world!' });

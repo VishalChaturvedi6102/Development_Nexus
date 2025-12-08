6a172e402e6c271c329f1eaae45afa16c3c2cf31
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2025-12-08 23:29:41 +0530

    commit

--- Javascript_/Practice/programs_basics/interview.js
+++ Javascript_/Practice/programs_basics/interview.js
@@ -433,6 +433,21 @@ fido.sayName(); // "My name is Fido"
 
 
 
+/**
+ * @param {Array} array - Array from which the elements are all numbers.
+ * @return {Number} Returns the mean.
+ */
+export default function mean(array) {
+  let total = 0;
+
+  // Calculate the sum of all numbers in the array.
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+
+  // Calculate the mean from the sum.
+  return total / array.length;
+}
 
 
 

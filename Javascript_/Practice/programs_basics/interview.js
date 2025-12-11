e3089977d5eec3ae3b4b84d7a35d2506901f7010
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2025-12-11 23:44:33 +0530

    commit

--- Javascript_/Practice/programs_basics/interview.js
+++ Javascript_/Practice/programs_basics/interview.js
@@ -491,6 +491,13 @@ console.log(humanize_format(402));
 
 
 
+// Function to clone an array
+var array_Clone = function(arra1) {
+  return arra1.slice(0);
+};
+
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
 
 
 

500ff8ecb2f4d40d52f99cc94736dc7b64dd5c1a
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2025-12-19 23:28:09 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -82,3 +82,18 @@ for (var x = 0; x < str.length; x++) {
 
 console.log(result.join(''));
 
+
+
+
+
+// Compute the sum and product of an array of integers
+var array = [1, 2, 3, 4, 5, 6];
+
+var s = 0;
+var p = 1;
+for (var i = 0; i < array.length; i += 1) {
+
+  s += array[i];
+  p *= array[i];
+}
+console.log('Sum : ' + s + ' Product :  ' + p); 

018a364fa9b9336eb9914e06064013282431129e
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-01-04 23:26:21 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -198,3 +198,28 @@ const differenceOf2Arrays = (array1, array2) => {
 };
 console.log(differenceOf2Arrays([1, 2, 3], [100, 2, 1, 10]));
 console.log(differenceOf2Arrays([1, 2, 3, 4, 5], [1, [2], [3, [[4]]], [5, 6]]));
+
+
+
+
+
+// Function to find two indices in the array 'nums' whose elements sum to the 'target_num'
+const twoSum = (nums, target_num) => {
+  const map = [];
+
+  const indexnum = [];
+
+  for (let x = 0; x < nums.length; x++) {
+    if (map[nums[x]] != null) {
+      const index = map[nums[x]];
+      indexnum[0] = index;
+      indexnum[1] = x;
+      break;
+    } else {
+      map[target_num - nums[x]] = x;
+    }
+  }
+  return indexnum;
+};
+
+console.log(twoSum([10, 20, 10, 40, 50, 60, 70], 50));

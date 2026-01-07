df8f7c248af06388d258fbd9f0dcac65178e31f5
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-01-07 23:43:31 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -250,6 +250,21 @@ console.log(result); // Output: [25, 10, 10, 1]
 
 
 
+// Define a function named insert that takes three parameters:
+insert = function insert(main_string, ins_string, pos) {
+   if(typeof(pos) == "undefined") {
+    pos = 0;
+  }
+
+   if(typeof(ins_string) == "undefined") {
+    ins_string = '';
+  }
+   return main_string.slice(0, pos) + ins_string + main_string.slice(pos);
+}
+
+console.log(insert('We are doing some exercises.'));
+console.log(insert('We are doing some exercises.','JavaScript '));
+console.log(insert('We are doing some exercises.','JavaScript ',18));
 
 
 

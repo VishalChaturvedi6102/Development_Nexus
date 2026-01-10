5f2e257d760803b49957f29ede3523393d110179
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-01-10 23:30:07 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -326,3 +326,25 @@ nums = [2, 4, 16, 32, 68];
 console.log("Original array: ", nums);
 console.log("Check the said array is a factor chain or not?");
 console.log(test(nums));
+
+
+
+
+// Function to find the union of two arrays
+const union = (arra1, arra2) => {
+  if ((arra1 == null) || (arra2 == null))
+    return void 0;
+  const obj = {};
+  for (let i = arra1.length - 1; i >= 0; --i)
+    obj[arra1[i]] = arra1[i];
+  for (let i = arra2.length - 1; i >= 0; --i)
+    obj[arra2[i]] = arra2[i];
+  const res = [];
+  for (let n in obj) {
+    if (obj.hasOwnProperty(n))
+      res.push(obj[n]);
+  }
+  return res;
+};
+
+console.log(union([1, 2, 3], [100, 2, 1, 10]));

743e27097dfec57ef8236319aac14e469adab91a
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-01-16 23:36:32 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -438,7 +438,7 @@ displayStack(stack) {
 console.log("Initialize a stack:")
 let stack = new Stack();
 console.log("Is the stack empty?");
-console.log(stack.isEmpty()); // true
+console.log(stack.isEmpty()); 
 console.log("Input some elements on the stack:")
 stack.push(1);
 stack.push(2);
@@ -460,3 +460,68 @@ console.log(stack.isEmpty());
 console.log("Size of the stack:");
 console.log(stack.size());
 
+
+
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  push(element) {
+    this.items.push(element);
+  }
+  pop() {
+    if (this.isEmpty()) {
+      return "Underflow";
+    }
+    return this.items.pop();
+  }
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  size() {
+    return this.items.length;
+  }
+  getMiddleElements() {
+    let middleIndex = Math.floor(this.size() / 2);
+    let middleElements = [];
+    if (this.size() % 2 === 0) {
+      middleElements.push(this.items[middleIndex - 1]);
+    }
+    middleElements.push(this.items[middleIndex]);
+    return middleElements;
+  }  
+  displayStack(stack) {
+  console.log("Stack elements are:");
+  let str = "";
+  for (let i = 0; i < stack.items.length; i++)
+    str += stack.items[i] + " ";
+  return str.trim();
+ }      
+}
+let stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+console.log(stack.displayStack(stack));
+console.log("Middle element(s):");
+console.log(stack.getMiddleElements());
+console.log("Add one more element:");
+stack.push(4);
+console.log(stack.displayStack(stack));
+console.log("Middle element(s):");
+console.log(stack.getMiddleElements());
+console.log("Add two more elements:");
+stack.push(5);
+stack.push(6);
+console.log(stack.displayStack(stack));
+console.log("Middle element(s):");
+console.log(stack.getMiddleElements());
+console.log("Add one more element:");
+stack.push(7);
+console.log(stack.displayStack(stack));
+console.log("Middle element(s):");
+console.log(stack.getMiddleElements());

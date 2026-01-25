54f2e9921f228f6dde3ac51a8efc1c0c6dadb17b
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-01-25 23:30:15 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -599,3 +599,27 @@ console.log(indexOn([
   { id: 10, name: 'apple' },
   { id: 20, name: 'orange' }
 ], x => x.id));
+
+
+
+
+
+
+
+
+// Function to check if all numbers in an array are prime
+function test(arr_nums) {
+  for (n of arr_nums) {
+    if (n == 1 || (n > 2 && n % 2 == 0)) 
+      return false;
+  }
+  return true;
+}
+
+nums = [2, 3, 5, 7];
+console.log("Original array of integers: " + nums);
+console.log("In the said array, check if every number is prime or not: " + test(nums));
+nums = [2, 3, 5, 7, 8];
+console.log("Original array of integers: " + nums);
+console.log("In the said array, check if every number is prime or not: " + test(nums));
+

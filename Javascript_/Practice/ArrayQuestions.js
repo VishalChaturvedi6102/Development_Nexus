5c079ba3e476275943e1d35220be1779e0d39e5d
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-01-29 23:41:46 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -673,3 +673,22 @@ function nthlargest(arra, highest) {
 }
 
 console.log(nthlargest([43, 56, 23, 89, 88, 90, 99, 652], 4));
+
+
+
+
+// Function to find the longest common starting substring in an array of strings
+const longest_common_starting_substring = (arr1) => {
+  const arr = [...arr1].sort();
+  const a1 = arr[0];
+  const a2 = arr[arr.length - 1];
+  const L = a1.length;
+  let i = 0;
+  while (i < L && a1.charAt(i) === a2.charAt(i)) {
+    i++;
+  }
+  return a1.substring(0, i);
+};
+console.log(longest_common_starting_substring(['go', 'google'])); // Output: 'go'
+console.log(longest_common_starting_substring(['SQLInjection', 'SQLTutorial'])); // Output: 'SQL'
+console.log(longest_common_starting_substring(['abcd', '1234'])); 

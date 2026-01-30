ef501b1a70c8546a7cb74fa53f0e0d6703fccb49
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-01-30 23:37:42 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -692,3 +692,28 @@ const longest_common_starting_substring = (arr1) => {
 console.log(longest_common_starting_substring(['go', 'google'])); // Output: 'go'
 console.log(longest_common_starting_substring(['SQLInjection', 'SQLTutorial'])); // Output: 'SQL'
 console.log(longest_common_starting_substring(['abcd', '1234'])); 
+
+
+
+
+
+// Function to move an element within an array from the 'old_index' to the 'new_index'
+function move(arr, old_index, new_index) {
+    while (old_index < 0) {
+        old_index += arr.length;
+    }
+    while (new_index < 0) {
+        new_index += arr.length;
+    }
+    if (new_index >= arr.length) {
+        let k = new_index - arr.length;
+        while ((k--) + 1) {
+            arr.push(undefined);
+        }
+    }
+
+    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    return arr;
+}
+console.log(move([10, 20, 30, 40, 50], 0, 2));
+console.log(move([10, 20, 30, 40, 50], -1, -2));

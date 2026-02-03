fd54f9bc06397f3368c39ce2f42b97140467192d
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-02-03 23:45:45 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -841,3 +841,23 @@ promisifiedFunction(2, 4)
   .catch(error => {
     console.log('Error:', error.message);
   });
+
+
+
+  // Generates all combinations of a string
+function substrings(str) {
+  const result = [];
+  function search_combination(start, curr) {
+    if (curr.length > 0) {
+      result.push(curr);
+    }
+    for (let i = start; i < str.length; i++) {
+      search_combination(i + 1, curr + str[i]);
+    }
+  }
+  search_combination(0, '');
+  return result;
+}
+const str = 'dog';
+const result = substrings(str);
+console.log(result); 

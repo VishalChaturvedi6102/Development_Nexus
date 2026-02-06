bf2d16513b6100d40167bb5ff2c99c4220ab09e5
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-02-06 23:40:20 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -909,3 +909,20 @@ function unique_char(str1) {
   return uniql;
 }
 console.log(unique_char("thequickbrownfoxjumpsoverthelazydog")); 
+
+
+
+
+// Function to generate an array of numbers in a specified range
+function array_range(start, len) 
+{
+  var arr = new Array(len);
+  for (var i = 0; i < len; i++, start++) 
+  {
+    arr[i] = start;
+  }
+  return arr;
+}
+
+console.log(array_range(1, 4));
+console.log(array_range(-6, 4));

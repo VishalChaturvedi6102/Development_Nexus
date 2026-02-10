c21a31e95fa9dd456a0243b4b1375bb2a3ee7bb2
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-02-10 23:46:09 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -983,3 +983,16 @@ function findFirstNotRepeatedChar(str) {
 }
 var result = findFirstNotRepeatedChar('abacddbec');
 console.log(result); 
+
+
+
+// Function to check if the first two elements of an array are either both 30 or both 40
+function twice3040(arra1) {
+    let a = arra1[0], 
+        b = arra1[1]; 
+    return (a === 30 && b === 30) || (a === 40 && b === 40); 
+}	
+console.log(twice3040([30, 30])); 
+console.log(twice3040([40, 40])); 
+console.log(twice3040([20, 20])); 
+console.log(twice3040([30])); 

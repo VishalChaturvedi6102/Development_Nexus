8c46d80d0f55abec1859c204d7beffa995b0ef79
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-02-13 23:41:40 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -1050,3 +1050,30 @@ function union(arra1, arra2) {
   return res;
 }
 console.log(union([1, 2, 3], [100, 2, 1, 10]));
+
+
+
+
+
+
+
+
+
+
+// Define a function named Second_Greatest_Lowest that finds the second smallest and second largest numbers in an array
+function Second_Greatest_Lowest(arr_num) {
+  arr_num.sort(function(x, y) {
+    return x - y;
+  });
+  var uniqa = [arr_num[0]];
+  var result = [];
+  for (var j = 1; j < arr_num.length; j++) {
+    if (arr_num[j - 1] !== arr_num[j]) {
+      uniqa.push(arr_num[j]);
+    }
+  }
+
+  result.push(uniqa[1], uniqa[uniqa.length - 2]);
+  return result.join(',');
+}
+console.log(Second_Greatest_Lowest([1, 2, 3, 4, 5])); 

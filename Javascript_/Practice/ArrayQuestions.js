e06f93b4264a5fb83147995928d14dd3951f0a2d
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-02-20 23:16:05 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -1145,3 +1145,16 @@ console.log(text_truncate('We are doing JS string exercises.',15,'!!'))
 
 
 
+// Generate an array between two integers of 1 step length
+function rangeBetween(start, end) 
+{
+  if (start > end) {
+    const arr = Array.from({ length: start - end + 1 }, (_, index) => start - index);
+    return arr;
+  } else {
+    const arr = Array.from({ length: end - start + 1 }, (_, index) => start + index);
+    return arr;
+  }
+}
+console.log(rangeBetween(4, 7));
+console.log(rangeBetween(-4, 7));

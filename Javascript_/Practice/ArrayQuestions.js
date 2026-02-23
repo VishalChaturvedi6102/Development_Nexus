3287ce40561e88b0b6665c9d1d0adfae43739b59
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-02-23 22:59:14 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -1204,3 +1204,22 @@ const Arrays_sum = (array1, array2) => {
   return result;
 };
 console.log(Arrays_sum([1, 0, 2, 3, 4], [3, 5, 6, 7, 8, 13]));
+
+
+
+
+// Find the longest common starting substring in a set of strings
+function longest_common_starting_substring(arr1) {
+  var arr = arr1.concat().sort(),
+      a1 = arr[0],
+      a2 = arr[arr.length - 1],
+      L = a1.length,
+      i = 0;
+  while (i < L && a1.charAt(i) === a2.charAt(i)) {
+    i++;
+  }
+  return a1.substring(0, i);
+}
+console.log(longest_common_starting_substring(['go', 'google'])); 
+console.log(longest_common_starting_substring(['SQLInjection', 'SQLTutorial'])); 
+console.log(longest_common_starting_substring(['abcd', '1234'])); 

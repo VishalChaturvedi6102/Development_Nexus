21b7c8114f8d08618e7e2ae1db59bbe57c510c1e
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-02-24 23:31:53 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -1223,3 +1223,18 @@ function longest_common_starting_substring(arr1) {
 console.log(longest_common_starting_substring(['go', 'google'])); 
 console.log(longest_common_starting_substring(['SQLInjection', 'SQLTutorial'])); 
 console.log(longest_common_starting_substring(['abcd', '1234'])); 
+
+
+
+
+// find the third smallest number in an array
+function test(arr_nums) {
+  return arr_nums.sort((x, y) => y - x)[arr_nums.length - 3];
+}
+nums = [2, 3, 5, 7, 1];
+console.log("Original array of numbers: " + nums);
+console.log("Third smallest number of the said array of numbers: " + test(nums));
+
+nums = [2, 3, 0, 5, 7, 8, -2, -4];
+console.log("Original array of numbers: " + nums);
+console.log("Third smallest number of the said array of numbers: " + test(nums));

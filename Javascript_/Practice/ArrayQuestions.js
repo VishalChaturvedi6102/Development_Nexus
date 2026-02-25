e61bdfe0d0e5c0948b48e371ebb7dc4a4b5bc358
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-02-25 23:14:25 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -1238,3 +1238,32 @@ console.log("Third smallest number of the said array of numbers: " + test(nums))
 nums = [2, 3, 0, 5, 7, 8, -2, -4];
 console.log("Original array of numbers: " + nums);
 console.log("Third smallest number of the said array of numbers: " + test(nums));
+
+
+
+
+
+
+
+
+// Retrieve the value of a specified property from all elements in an array
+var librarys = [ 
+  { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+  { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+  { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+];
+function property_value(array, property_key) {
+  var arr = [],
+    index = -1,
+    arrlen = array.length,
+    array_items;
+  while (++index < arrlen) {
+    array_items = array[index];
+    if (array_items.hasOwnProperty(property_key)) {
+      arr[arr.length] = array_items[property_key];
+    }
+  }
+  return arr;
+}
+console.log(property_value(library, 'title'));
+console.log(property_value(library, 'author'));

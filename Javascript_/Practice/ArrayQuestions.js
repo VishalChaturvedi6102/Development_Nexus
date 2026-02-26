bf60d1bcd424e00b184c2a15b3c0d455481822bf
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-02-26 23:34:22 +0530

    commit

--- Javascript_/Practice/ArrayQuestions.js
+++ Javascript_/Practice/ArrayQuestions.js
@@ -1267,3 +1267,25 @@ function property_value(array, property_key) {
 }
 console.log(property_value(library, 'title'));
 console.log(property_value(library, 'author'));
+
+
+
+
+// Move an array element from one position to another
+function move(arr, old_index, new_index) {
+    while (old_index < 0) {
+        old_index += arr.length;
+    }
+    while (new_index < 0) {
+        new_index += arr.length;
+    }
+    if (new_index >= arr.length) {
+        var k = new_index - arr.length;
+        while ((k--) + 1) {
+            arr.push(undefined);
+        } }
+    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    return arr;
+}
+console.log(move([10, 20, 30, 40, 50], 0, 2));
+console.log(move([10, 20, 30, 40, 50], -1, -2));

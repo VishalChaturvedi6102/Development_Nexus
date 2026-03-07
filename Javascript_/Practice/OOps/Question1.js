4b7246332a1ea502cd911bb8398ec09ad9a410a6
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-03-07 23:30:35 +0530

    commit

---
+++ Javascript_/Practice/OOps/Question1.js
@@ -0,0 +1,27 @@
+// Person Class with Details
+
+// Write a JavaScript program to create a class called "Person" with properties for name, age and country. Include a method to display the person's details. Create two instances of the 'Person' class and display their details.
+
+class Person {
+  constructor(name, age, country) {
+    this.name = name;
+    this.age = age;
+    this.country = country;
+  }
+
+  displayDetails() {
+    console.log(`Name: ${this.name}`);
+    console.log(`Age: ${this.age}`);
+    console.log(`Country: ${this.country}`);
+  }
+}
+
+
+const person1 = new Person('Francisca Rohan', 25, 'USA');
+const person2 = new Person('Raimond Aruna', 30, 'Netherlands');
+
+console.log('Person-1 Details:');
+person1.displayDetails();
+
+console.log('\nPerson-2 Details:');
+person2.displayDetails();

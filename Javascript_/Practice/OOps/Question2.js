2714a6f32f7102d983b4a31c337c02f986586e6b
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-03-08 22:57:55 +0530

    commit

---
+++ Javascript_/Practice/OOps/Question2.js
@@ -0,0 +1,27 @@
+// Rectangle with area and perimeter methods
+
+// Write a JavaScript program to create a class called 'Rectangle' with properties for width and height. Include two methods to calculate rectangle area and perimeter. Create an instance of the 'Rectangle' class and calculate its area and perimeter.
+
+
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  calculateArea() {
+    return this.width * this.height;
+  }
+
+  calculatePerimeter() {
+    return 2 * (this.width + this.height);
+  }
+}
+const rectangle = new Rectangle(12, 10);
+
+const area = rectangle.calculateArea();
+const perimeter = rectangle.calculatePerimeter();
+
+console.log(`Rectangle Area: ${area}`);
+console.log(`Rectangle Perimeter: ${perimeter}`);

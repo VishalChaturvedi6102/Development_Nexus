c6cc15a0caa5dbdccceb78bda7b2b2b9a3b0f6ad
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-03-24 23:10:12 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question6.js
@@ -0,0 +1,21 @@
+// Define a function named max that takes an input array.
+
+// Find Maximum in Array
+
+// Write a JavaScript function to find the highest value in an array.Mathematics
+
+// Test Data :
+// console.log(max([12,34,56,1]));
+// console.log(max([-12,-34,0,-56,-1]));
+// 56
+// 0
+
+
+function max(input) {
+    if (toString.call(input) !== "[object Array]")
+        return false;
+    return Math.max.apply(null, input);
+}
+
+console.log(max([12, 34, 56, 1]));
+console.log(max([-12, -34, 0, -56, -1]));

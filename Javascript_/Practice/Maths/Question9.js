641dc0898de30da43730664086ef9b1bafb6f907
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-03-27 23:32:24 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question9.js
@@ -0,0 +1,45 @@
+// Define a function named gcd_more_than_two_numbers that calculates the greatest common divisor (GCD) of an array of numbers.
+
+// GCD of Multiple Numbers
+
+// Write a JavaScript function to find the GCD (greatest common divisor) of more than 2 integers.
+
+// Test Data:
+// console.log(gcd_more_than_two_numbers([3,15,27]));
+// console.log(gcd_more_than_two_numbers([5,10,15,25]));
+// Output :
+// 3
+// 5
+
+
+function gcd_more_than_two_numbers(input) {
+    if (toString.call(input) !== "[object Array]")
+        return false;
+    
+    var len, a, b;
+    len = input.length;
+    if (!len) {
+        return null;
+    }
+    a = input[0];
+    for (var i = 1; i < len; i++) {
+        b = input[i];
+        a = gcd_two_numbers(a, b);
+    }
+    return a;
+}
+function gcd_two_numbers(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number')) 
+        return false;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while(y) {
+        var t = y;
+        y = x % y;
+        x = t;
+    }
+    return x;
+}
+
+console.log(gcd_more_than_two_numbers([3, 15, 27]));
+console.log(gcd_more_than_two_numbers([5, 10, 15, 25]));

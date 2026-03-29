ffbc984bdf1924bb793a5940a04d1fc8622962ec
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-03-29 23:14:07 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question11.js
@@ -0,0 +1,41 @@
+// Define a function named lcm_more_than_two_numbers that calculates the least common multiple (LCM) of an array of numbers.
+
+
+// LCM of Multiple Numbers
+
+// Write a JavaScript function to get the least common multiple (LCM) of more than 2 integers.
+
+// Test Data :
+// console.log(lcm_more_than_two_numbers([100,90,80,7]));
+// console.log(lcm_more_than_two_numbers([5,10,15,25]));
+// Output :
+// 25200
+// 150
+
+
+
+function lcm_more_than_two_numbers(input_array) {
+    if (toString.call(input_array) !== "[object Array]")  
+        return false;  
+    
+    var r1 = 0, r2 = 0;
+    var l = input_array.length;
+    for(i = 0; i < l; i++) {
+        r1 = input_array[i] % input_array[i + 1];
+        if (r1 === 0) {
+            input_array[i + 1] = (input_array[i] * input_array[i + 1]) / input_array[i + 1];
+        }
+        else {
+            r2 = input_array[i + 1] % r1;
+            if (r2 === 0) {
+                input_array[i + 1] = (input_array[i] * input_array[i + 1]) / r1;
+            }
+            else {
+                input_array[i + 1] = (input_array[i] * input_array[i + 1]) / r2;
+            }
+        }
+    }
+    return input_array[l - 1];
+}
+console.log(lcm_more_than_two_numbers([100, 90, 80, 7])); 
+console.log(lcm_more_than_two_numbers([5, 10, 15, 25]));

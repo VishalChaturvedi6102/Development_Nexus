64eb4dff5072e32e7fed3c72768ee50228c37c66
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-04-09 23:15:33 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question20.js
@@ -0,0 +1,30 @@
+// Define a function named binomial that calculates the binomial coefficient "n choose k".
+
+// Binomial Coefficients
+
+// Write a JavaScript program to evaluate binomial coefficients.
+
+// Note:
+// Binomial coefficient : According to Wikipedia - In mathematics, binomial coefficients are a family of positive integers that occur as coefficients in the binomial theorem. They are indexed by two nonnegative integers; the binomial coefficient indexed by n and k is usually written \tbinom nk. It is the coefficient of the x k term in the polynomial expansion of the binomial power (1 + x) n. Under suitable circumstances the value of the coefficient is given by the expression :
+
+// Binomial coefficients
+// Arranging binomial coefficients into rows for successive values of n, and in which k ranges from 0 to n, gives a triangular array called Pascal's triangle.
+
+// Test Data:
+// console.log(binomial(8,3));
+// console.log(binomial(10,2));
+// Output :
+// 56
+// 45
+
+function binomial(n, k) {
+    if ((typeof n !== 'number') || (typeof k !== 'number')) 
+        return false; 
+    var coeff = 1;
+    for (var x = n - k + 1; x <= n; x++) coeff *= x;
+    for (x = 1; x <= k; x++) coeff /= x;
+    return coeff;
+}
+
+console.log(binomial(8, 3));
+console.log(binomial(10, 2));

0222821ba2472680444a37809c061653e75c725c
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-04-13 23:04:09 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question24.js
@@ -0,0 +1,22 @@
+// Declare a variable named a and assign it a floating-point number value with trailing zeros.
+// Round and Strip Extra Zeros
+// Write a JavaScript function to round a number to a specified number of digits and strip extra zeros (if any).
+// Test Data :
+// var a = -4.55555;
+// console.log(result);
+// -4.5556
+// var a = 5.0001000;
+// console.log(result);
+// 5.0001
+
+
+
+// var a = -1.1230000;
+// var a = 0.0000009999999;
+// var a = 1.000000000;
+// var a = -4.55555;
+// var a = 5.0001000;
+var a = 1.1234000;
+
+var result = parseFloat(a.toFixed(4));
+console.log(result);

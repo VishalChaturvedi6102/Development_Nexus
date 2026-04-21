4bba8e2024dc2fc8e7ed8c33f2e6f9971687a14c
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-04-21 23:01:32 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question32.js
@@ -0,0 +1,12 @@
+// Define a function named percentage that calculates the percentage of a given number.
+// Calculate Percentage of a Number
+// Write a JavaScript function to calculate the percentage (%) of a number.
+// Test Data:
+// console.log(percentage(1000, 47.12));
+// 471.2
+
+function percentage(num, per)
+{
+  return (num/100)*per;
+}
+console.log(percentage(1000, 47.12));

4e4a3f063ebc6547d5aa3cc75b2487699dbf3142
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-04-29 23:41:04 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question40.js
@@ -0,0 +1,12 @@
+// Define a function named random_bg_color that generates a random RGB color and sets it as the background color of the document body.
+
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    console.log(bgColor);
+    document.body.style.background = bgColor;
+}
+random_bg_color();

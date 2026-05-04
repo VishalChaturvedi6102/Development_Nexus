319a37e4e2f0c814909a3096af5f9ee19d4974bb
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-05-04 23:37:50 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question45.js
@@ -0,0 +1,27 @@
+// Define a function named subtraction that performs subtraction on an array of numbers.
+// Subtract Elements from Array
+// Write a JavaScript function to subtract elements from an array.
+
+
+function subtraction(arr) 
+{
+  if (Object.prototype.toString.call(arr) === '[object Array]') {
+    var total = arr[0];
+    if (typeof (total) !== 'number') {
+      return false;
+    }
+    for (var i = 1, length = arr.length; i < length; i++)
+    {
+      if (typeof (arr[i]) === 'number')
+      {
+        total -= arr[i];
+      } 
+      else 
+        return false;
+    }
+    return total;
+  } 
+  else
+    return false;
+}
+console.log(subtraction([7,3, 2,-1]));

a6ca6809d72eb4f4ffac968d2db14c95f65ffc6a
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-06-10 23:22:23 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question79.js
@@ -0,0 +1,31 @@
+// Dots in Triangular Number
+// Write a JavaScript program to get the number of dots based on the triangular number of the sequence. 
+// A triangular number or triangle number counts objects arranged in an equilateral triangle. Triangular numbers are a type of figurate number, other examples being square numbers and cube numbers. The nth triangular number is the number of dots in the triangular arrangement with n dots on each side, and is equal to the sum of the n natural numbers from 1 to n. The sequence of triangular numbers, starting with the 0th triangular number, is
+// 0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91, 105, 120, 136, 153, 171, 190, 210, 231, 253, 276, 300, 325, 351, 378, 406, 435, 465, 496, 528, 561, 595, 630, 666...
+// Test Data:
+// (1) -> 1
+// (2) -> 3
+// (3) -> 6
+// (7) -> 28
+// (11) -> 66
+
+
+function test(n) {
+    return n * (n + 1) / 2;
+}
+
+n = 1;
+console.log("Triangular number = " + n);
+console.log("Number of dots based on the triangular number of the sequence = " + test(n));
+n = 2;
+console.log("Triangular number = " + n);
+console.log("Number of dots based on the triangular number of the sequence = " + test(n));
+n = 3;
+console.log("Triangular number = " + n);
+console.log("Number of dots based on the triangular number of the sequence = " + test(n));
+n = 7;
+console.log("Triangular number = " + n);
+console.log("Number of dots based on the triangular number of the sequence = " + test(n));
+n = 11;
+console.log("Triangular number = " + n);
+console.log("Number of dots based on the triangular number of the sequence = " + test(n));

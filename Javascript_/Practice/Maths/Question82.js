b6e80375156afb7402136368290de0654274e70c
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-06-13 23:37:09 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question82.js
@@ -0,0 +1,31 @@
+// Mean of Digits in a Number
+// Write a JavaScript program to calculate the mean of all the digits of a given number. Mathematics
+// What is a Mean?
+// In  mathematics, the mean represents the simple average of two or more numbers. There are several methods available for computing the mean of a set of numbers, including the arithmetic mean method, which uses the sum of the numbers in the series, and the geometric mean method, which is the average of a set of products.
+// Test Data:
+// (11) -> 1
+// (66) -> 6
+// (336) -> 4
+// (444) -> 4
+// (1151) -> 2
+
+
+function test(n) {
+    let arr_str = String(Math.abs(n)).split('');
+    return arr_str.reduce((a, b) => a + Number(b), 0) / arr_str.length;
+}
+n = 11;
+console.log("n = " + n);
+console.log("Mean of all digits of the said number: " + test(n));
+n = 66;
+console.log("n = " + n);
+console.log("Mean of all digits of the said number: " + test(n));
+n = 336;
+console.log("n = " + n);
+console.log("Mean of all digits of the said number: " + test(n));
+n = 444;
+console.log("n = " + n);
+console.log("Mean of all digits of the said number: " + test(n));
+n = 1151;
+console.log("n = " + n);
+console.log("Mean of all digits of the said number: " + test(n));

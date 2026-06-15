ae11bb4108096c66973dafbc73343118cb424e59
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-06-15 22:38:08 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question84.js
@@ -0,0 +1,19 @@
+// Sum of Two Highest Numbers in Array
+// Write a JavaScript program to calculate the sum of the two highest positive numbers in a given array.
+// Test Data:
+// ([1,2,6,3,4,5,6,7]) ->13
+// ([2,3,4,5]) -> 9
+
+function test(nums) {
+  return nums.filter((el) => el >= 0) 
+    .sort((x, y) => x - y) 
+    .slice(-2) 
+    .reduce((acc, el) => acc + el); 
+}
+
+nums = [1, 2, 6, 3, 4, 5, 6, 7];
+console.log("nums = " + nums);
+console.log("Sum of the two highest numbers of the said array: " + test(nums));
+nums = [2, 3, 4, 5];
+console.log("nums = " + nums);
+console.log("Sum of the two highest numbers of the said array: " + test(nums));

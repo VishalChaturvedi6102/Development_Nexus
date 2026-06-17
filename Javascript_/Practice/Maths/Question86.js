8d8e4eb5fa620454767751e81e76a938be5da441
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-06-17 23:13:19 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question86.js
@@ -0,0 +1,29 @@
+// Get Middle Character(s) of String
+// Write a JavaScript program to get the middle character(s) from a given string.
+// Test Data:
+// ("abcd") -> "bc"
+// ("abc") -> "b"
+// ("JavaScript") -> "Sc"
+
+
+function test(text) {
+   
+    var text_len = text.length;
+    if (text_len % 2 != 0) {
+        let start = (text_len - 1) / 2;
+        return text.slice(start, start + 1);
+    } else {
+        let start = text_len / 2 - 1;
+        return text.slice(start, start + 2);
+    }
+}
+
+text = "abcd";
+console.log("Original string: " + text);
+console.log("Middle character(s) of the said string: " + test(text));
+text = "abc";
+console.log("Original string: " + text);
+console.log("Middle character(s) of the said string: " + test(text));
+text = "JavaScript";
+console.log("Original string: " + text);
+console.log("Middle character(s) of the said string: " + test(text));

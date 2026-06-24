2fcac05d62ff04d93a30f691232c8cef75290747
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-06-24 23:10:22 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question93.js
@@ -0,0 +1,22 @@
+// Check Downward Trend in Array
+// Write a JavaScript program to check if an array of integers has a downward trend or not.
+// Test Data:
+// ([1, 3, 4, 7, 9, 10, 11]) -> false
+// ([11, 10, 9, 7, 4, 3, 2, 0]) -> true
+// ([1, 0, -2, -3, -12]) -> true
+
+
+function test(nums) {
+    return nums.every((item, i) => item > nums[i + 1] || item === nums[nums.length - 1]);
+}
+
+// Test cases
+let nums = [1, 3, 4, 7, 9, 10, 11];
+console.log("n = " + nums);
+console.log("Process: " + test(nums));
+nums = [11, 10, 9, 7, 4, 3, 2, 0];
+console.log("n = " + nums);
+console.log("Process: " + test(nums));
+nums = [1, 0, -2, -3, -12];
+console.log("n = " + nums);
+console.log("Process: " + test(nums));

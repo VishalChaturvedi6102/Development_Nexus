6a7930d18fce3e932839a616cb49473c62289397
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-07-06 23:09:07 +0530

    commit

---
+++ Javascript_/Practice/Maths/Question104.js
@@ -0,0 +1,29 @@
+// Climbing Stairs
+// A staircase consists of N steps, and you are given the choice of climbing one step at a time or two steps at a time.
+// Write a JavaScript program to find distinct ways to climb the staircase. Home Improvement
+// Example:
+// Number of stairs = 2
+// There are 2 ways to climb the stairs: (1,1) and (2)
+// Number of stairs = 3
+// There are 3 ways to climb the stairs: (1,1,1), (2,1) and (1,2)
+
+
+function test(n) {
+    var x = 0, 
+        y = 1; 
+    
+    while (n-- > 0) {
+        y = x + y; 
+        x = y - x; 
+    }
+    
+    return y; 
+}
+
+n = 2;
+console.log("Number of stairs = " + n);
+console.log("Number of ways to climb the said number of stairs: " + test(n));
+
+n = 3;
+console.log("Number of stairs = " + n);
+console.log("Number of ways to climb the said number of stairs: " + test(n));

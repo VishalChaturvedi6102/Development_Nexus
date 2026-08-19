56bc1dcb2023dc3f1b6cf79c959f98836a3a5a61
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-08-19 23:17:32 +0530

    commit

---
+++ Javascript_/Practice/Linked_List/Doubly_Linked_List/Question15.js
@@ -0,0 +1,80 @@
+// Remove the head element from a DLL
+// Write a JavaScript program that removes the head element from a doubly Linked lists.
+// o/p :
+// Original Doubly Linked Lists:
+//  2 3 4 5 6 7
+// Remove the Head element:
+// 2 3 4 5 6 7
+// Remove the Head element:
+// 3 4 5 6 7
+
+
+class Node {
+         constructor(value) {
+                  this.value = value;
+                  this.next = null;
+                  this.previous = null;
+         }
+}
+class DoublyLinkedList {
+         constructor(value) {
+                  this.head = {
+                            value: value,
+                            next: null,
+                            previous: null
+                  };
+                  this.length = 0;
+                  this.tail = this.head;
+         }
+ add(newNode) {
+   if (this.head === null) {
+   this.head = newNode;
+   this.tail = newNode;
+   }
+   else
+   {
+   newNode.previous = this.tail;
+   this.tail.next = newNode;
+   this.tail = newNode;
+   }
+    this.length++;
+  }
+remove_Head() {
+   if (!this.head) {
+     return null;
+   }
+   if (!this.head.next) {
+     this.head = null;
+     this.tail = null;
+     return;
+   }
+   this.head = this.head.next;
+   this.head.previous = null;
+   this.length--;
+ }
+ printList(){
+                  let current = this.head;
+                  let result = [];
+                  while (current !== null) {
+                            result.push(current.value);
+                            current = current.next;
+                  }
+                  console.log(result.join(' '));
+                  return this;
+          }
+}
+let numList = new DoublyLinkedList();
+numList.add(new Node(2));
+numList.add(new Node(3));
+numList.add(new Node(4));
+numList.add(new Node(5));
+numList.add(new Node(6));
+numList.add(new Node(7));
+console.log("Original Doubly Linked Lists:")
+numList.printList();
+console.log("Remove the Head element:");
+numList.remove_Head();
+numList.printList();
+console.log("Remove the Head element:");
+numList.remove_Head();
+numList.printList();

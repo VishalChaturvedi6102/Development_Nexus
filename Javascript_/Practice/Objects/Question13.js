103e079a39b99a10dc10e64592c981dd0f248c99
Vishal Chaturvedi <vishalchaturvedi6102@gmail.com>
2026-09-16 23:36:27 +0530

    commit

---
+++ Javascript_/Practice/Objects/Question13.js
@@ -0,0 +1,23 @@
+// Object Properties (Own & Inherited)
+// Write a JavaScript function to retrieve all the names of an object's own and inherited properties.
+
+// O/P :
+// ["name","rollno"]
+
+
+function allKeys(obj) {
+    if (!isObject(obj)) return [];
+    var keys = [];
+    for (var key in obj) keys.push(key);
+    return keys;
+}
+function isObject(obj) 
+{
+    var type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj;
+  }
+function Student(name) {
+  this.name = name;
+}
+Student.prototype.rollno = true;
+console.log(allKeys(new Student("Sara")));
